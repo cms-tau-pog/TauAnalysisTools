703aede6b6d695a6ef161374b28b0cf5462dcd96
Aruna Nayak <Aruna.Nayak@cern.ch>
2015-09-23 16:55:09 +0200

    update mva writing tools

---
+++ TauAnalysisTools/plugins/GBRForestWriter.h
@@ -0,0 +1,112 @@
+#ifndef RecoTauTag_TauAnalysisTools_GBRForestWriter_h
+#define RecoTauTag_TauAnalysisTools_GBRForestWriter_h
+
+/** \class GBRForestWriter
+ *
+ * Read GBRForest objects from ROOT file input
+ * and store it in SQL-lite output file
+ *
+ * \author Christian Veelken, LLR
+ *
+ */
+
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include <vector>
+#include <string>
+
+class GBRForestWriter : public edm::EDAnalyzer 
+{
+ public:
+  GBRForestWriter(const edm::ParameterSet&);
+  ~GBRForestWriter();
+  
+ private:
+  virtual void analyze(const edm::Event&, const edm::EventSetup&);
+
+  std::string moduleLabel_;
+
+  bool hasRun_;
+
+  typedef std::vector<std::string> vstring;
+
+  struct categoryEntryType
+  {
+    categoryEntryType(const edm::ParameterSet& cfg)
+    {
+      if ( cfg.existsAs<edm::FileInPath>("inputFileName") ) {
+	edm::FileInPath inputFileName_fip = cfg.getParameter<edm::FileInPath>("inputFileName");
+	if ( inputFileName_fip.location()!=edm::FileInPath::Local)
+	  throw cms::Exception("GBRForestWriter") 
+	    << " Failed to find File = " << inputFileName_fip << " !!\n";
+	inputFileName_ = inputFileName_fip.fullPath();
+      } else if ( cfg.existsAs<std::string>("inputFileName") ) {
+	inputFileName_ = cfg.getParameter<std::string>("inputFileName");
+      } else throw cms::Exception("GBRForestWriter") 
+	  << " Undefined Configuration Parameter 'inputFileName !!\n";
+      std::string inputFileType_string = cfg.getParameter<std::string>("inputFileType");
+      if      ( inputFileType_string == "XML"       ) inputFileType_ = kInputXML;
+      else if ( inputFileType_string == "GBRForest" ) inputFileType_ = kInputGBRForest;
+      else throw cms::Exception("GBRForestWriter") 
+	<< " Invalid Configuration Parameter 'inputFileType' = " << inputFileType_string << " !!\n";
+      if ( inputFileType_ == kInputXML ) {
+	inputVariables_ = cfg.getParameter<vstring>("inputVariables");
+	spectatorVariables_ = cfg.getParameter<vstring>("spectatorVariables");
+      }
+      gbrForestName_ = cfg.getParameter<std::string>("gbrForestName");
+    }
+    ~categoryEntryType() {}
+    std::string inputFileName_;
+    enum { kInputUndefined, kInputXML, kInputGBRForest };
+    int inputFileType_;
+    vstring inputVariables_;
+    vstring spectatorVariables_;
+    std::string gbrForestName_;
+  };
+  struct jobEntryType
+  {
+    jobEntryType(const edm::ParameterSet& cfg)
+    {
+      if ( cfg.exists("categories") ) {
+	edm::VParameterSet cfgCategories = cfg.getParameter<edm::VParameterSet>("categories");
+	for ( edm::VParameterSet::const_iterator cfgCategory = cfgCategories.begin();
+	      cfgCategory != cfgCategories.end(); ++cfgCategory ) {
+	  categoryEntryType* category = new categoryEntryType(*cfgCategory);
+	  categories_.push_back(category);
+	} 
+      } else {
+	categoryEntryType* category = new categoryEntryType(cfg);
+	categories_.push_back(category);
+      }
+      std::string outputFileType_string = cfg.getParameter<std::string>("outputFileType");
+      if      ( outputFileType_string == "GBRForest" ) outputFileType_ = kOutputGBRForest;
+      else if ( outputFileType_string == "SQLLite"   ) outputFileType_ = kOutputSQLLite;
+      else throw cms::Exception("GBRForestWriter") 
+	<< " Invalid Configuration Parameter 'outputFileType' = " << outputFileType_string << " !!\n";
+      if ( outputFileType_ == kOutputGBRForest ) {
+	outputFileName_ = cfg.getParameter<std::string>("outputFileName");
+      } 
+      if ( outputFileType_ == kOutputSQLLite ) {
+	outputRecord_ = cfg.getParameter<std::string>("outputRecord");
+      } 
+    }
+    ~jobEntryType() 
+    {
+      for ( std::vector<categoryEntryType*>::iterator it = categories_.begin();
+	    it != categories_.end(); ++it ) {
+	delete (*it);
+      }
+    }
+    std::vector<categoryEntryType*> categories_;
+    enum { kOutputUndefined, kOutputGBRForest, kOutputSQLLite };
+    int outputFileType_;
+    std::string outputFileName_;
+    std::string outputRecord_;
+  };
+  std::vector<jobEntryType*> jobs_;
+};
+
+#endif
--- TauAnalysisTools/python/writeTauIdDiscrMVA_cfi.py
+++ TauAnalysisTools/python/writeTauIdDiscrMVA_cfi.py
@@ -9,29 +9,43 @@ writeTauIdDiscrMVAs = cms.EDAnalyzer("GBRForestWriter",
             inputFileType = cms.string("GBRForest"),
             gbrForestName = cms.string("tauIdMVAIsoDBoldDMwLT"),
             outputFileType = cms.string("SQLLite"),                                      
-            outputRecord = cms.string("tauIdMVAIsoDBoldDMwLT")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/gbrDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             inputFileType = cms.string("GBRForest"),            
             gbrForestName = cms.string("tauIdMVAIsoDBnewDMwLT"),
             outputFileType = cms.string("SQLLite"),                                      
-            outputRecord = cms.string("tauIdMVAIsoDBnewDMwLT")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/gbrDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             inputFileType = cms.string("GBRForest"),                                       
             gbrForestName = cms.string("tauIdMVAIsoPWoldDMwLT"),
             outputFileType = cms.string("SQLLite"),                                     
-            outputRecord = cms.string("tauIdMVAIsoPWoldDMwLT")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/gbrDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             inputFileType = cms.string("GBRForest"),                                       
             gbrForestName = cms.string("tauIdMVAIsoPWnewDMwLT"),
             outputFileType = cms.string("SQLLite"), 
-            outputRecord = cms.string("tauIdMVAIsoPWnewDMwLT")
-        )                                             
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/gbrDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            inputFileType = cms.string("GBRForest"),
+            gbrForestName = cms.string("tauIdMVAIsoDBR03oldDMwLT"),
+            outputFileType = cms.string("SQLLite"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/gbrDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            inputFileType = cms.string("GBRForest"),
+            gbrForestName = cms.string("tauIdMVAIsoPWR03oldDMwLT"),
+            outputFileType = cms.string("SQLLite"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1")
+        )
     )
 )
 
@@ -42,122 +56,182 @@ writeTauIdDiscrWPs = cms.EDAnalyzer("TGraphWriter",
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBoldDMwLT.root'),
             graphName = cms.string("isoDBoldDMwLTEff90"),
-            outputRecord = cms.string("isoDBoldDMwLTEff90")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1_WPEff90")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBoldDMwLT.root'),
             graphName =cms.string("isoDBoldDMwLTEff80"),
-            outputRecord = cms.string("isoDBoldDMwLTEff80")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1_WPEff80")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBoldDMwLT.root'),
             graphName =cms.string("isoDBoldDMwLTEff70"),
-            outputRecord = cms.string("isoDBoldDMwLTEff70")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1_WPEff70")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBoldDMwLT.root'),
             graphName =cms.string("isoDBoldDMwLTEff60"),
-            outputRecord = cms.string("isoDBoldDMwLTEff60")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1_WPEff60")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBoldDMwLT.root'),
             graphName =cms.string("isoDBoldDMwLTEff50"),
-            outputRecord = cms.string("isoDBoldDMwLTEff50")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1_WPEff50")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBoldDMwLT.root'),
             graphName =cms.string("isoDBoldDMwLTEff40"),
-            outputRecord = cms.string("isoDBoldDMwLTEff40")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1_WPEff40")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             graphName =cms.string("isoDBnewDMwLTEff90"),
-            outputRecord = cms.string("isoDBnewDMwLTEff90")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1_WPEff90")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             graphName =cms.string("isoDBnewDMwLTEff80"),
-            outputRecord = cms.string("isoDBnewDMwLTEff80")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1_WPEff80")
             ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             graphName =cms.string("isoDBnewDMwLTEff70"),
-            outputRecord = cms.string("isoDBnewDMwLTEff70")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1_WPEff70")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             graphName =cms.string("isoDBnewDMwLTEff60"),
-            outputRecord = cms.string("isoDBnewDMwLTEff60")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1_WPEff60")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             graphName =cms.string("isoDBnewDMwLTEff50"),
-            outputRecord = cms.string("isoDBnewDMwLTEff50")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1_WPEff50")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             graphName =cms.string("isoDBnewDMwLTEff40"),
-            outputRecord = cms.string("isoDBnewDMwLTEff40")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1_WPEff40")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             graphName =cms.string("isoPWoldDMwLTEff90"),
-            outputRecord = cms.string("isoPWoldDMwLTEff90")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1_WPEff90")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             graphName =cms.string("isoPWoldDMwLTEff80"),
-            outputRecord = cms.string("isoPWoldDMwLTEff80")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1_WPEff80")
             ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             graphName =cms.string("isoPWoldDMwLTEff70"),
-            outputRecord = cms.string("isoPWoldDMwLTEff70")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1_WPEff70")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             graphName =cms.string("isoPWoldDMwLTEff60"),
-            outputRecord = cms.string("isoPWoldDMwLTEff60")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1_WPEff60")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             graphName =cms.string("isoPWoldDMwLTEff50"),
-            outputRecord = cms.string("isoPWoldDMwLTEff50")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1_WPEff50")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             graphName =cms.string("isoPWoldDMwLTEff40"),
-            outputRecord = cms.string("isoPWoldDMwLTEff40")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1_WPEff40")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             graphName =cms.string("isoPWnewDMwLTEff90"),
-            outputRecord = cms.string("isoPWnewDMwLTEff90")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1_WPEff90")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             graphName =cms.string("isoPWnewDMwLTEff80"),
-            outputRecord = cms.string("isoPWnewDMwLTEff80")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1_WPEff80")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             graphName =cms.string("isoPWnewDMwLTEff70"),
-            outputRecord = cms.string("isoPWnewDMwLTEff70")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1_WPEff70")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             graphName =cms.string("isoPWnewDMwLTEff60"),
-            outputRecord = cms.string("isoPWnewDMwLTEff60")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1_WPEff60")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             graphName =cms.string("isoPWnewDMwLTEff50"),
-            outputRecord = cms.string("isoPWnewDMwLTEff50")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1_WPEff50")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             graphName =cms.string("isoPWnewDMwLTEff40"),
-            outputRecord = cms.string("isoPWnewDMwLTEff40")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1_WPEff40")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            graphName = cms.string("isoDBR03oldDMwLTEff90"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1_WPEff90")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            graphName =cms.string("isoDBR03oldDMwLTEff80"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1_WPEff80")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            graphName =cms.string("isoDBR03oldDMwLTEff70"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1_WPEff70")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            graphName =cms.string("isoDBR03oldDMwLTEff60"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1_WPEff60")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            graphName =cms.string("isoDBR03oldDMwLTEff50"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1_WPEff50")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            graphName =cms.string("isoDBR03oldDMwLTEff40"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1_WPEff40")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            graphName = cms.string("isoPWR03oldDMwLTEff90"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1_WPEff90")
+            ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            graphName =cms.string("isoPWR03oldDMwLTEff80"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1_WPEff80")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            graphName =cms.string("isoPWR03oldDMwLTEff70"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1_WPEff70")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            graphName =cms.string("isoPWR03oldDMwLTEff60"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1_WPEff60")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            graphName =cms.string("isoPWR03oldDMwLTEff50"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1_WPEff50")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            graphName =cms.string("isoPWR03oldDMwLTEff40"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1_WPEff40")
         )
     )
 )
@@ -169,22 +243,32 @@ writeTauIdDiscrMVAoutputNormalizations = cms.EDAnalyzer("TFormulaWriter",
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBoldDMwLT.root'),
             formulaName = cms.string("mvaOutput_normalization_isoDBoldDMwLT"),
-            outputRecord = cms.string("mvaOutput_normalization_isoDBoldDMwLT")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBoldDMwLTv1_mvaOutput_normalization")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBnewDMwLT.root'),
             formulaName = cms.string("mvaOutput_normalization_isoDBnewDMwLT"),
-            outputRecord = cms.string("mvaOutput_normalization_isoDBnewDMwLT")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBnewDMwLTv1_mvaOutput_normalization")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWoldDMwLT.root'),
             formulaName = cms.string("mvaOutput_normalization_isoPWoldDMwLT"),
-            outputRecord = cms.string("mvaOutput_normalization_isoPWoldDMwLT")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWoldDMwLTv1_mvaOutput_normalization")
         ),
         cms.PSet(
             inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWnewDMwLT.root'),
             formulaName = cms.string("mvaOutput_normalization_isoPWnewDMwLT"),
-            outputRecord = cms.string("mvaOutput_normalization_isoPWnewDMwLT")
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWnewDMwLTv1_mvaOutput_normalization")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoDBR03oldDMwLT.root'),
+            formulaName = cms.string("mvaOutput_normalization_isoDBR03oldDMwLT"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoDBR03oldDMwLTv1_mvaOutput_normalization")
+        ),
+        cms.PSet(
+            inputFileName = cms.FileInPath('TauAnalysisTools/TauAnalysisTools/data/wpDiscriminationByIsolationMVA1Run2_isoPWR03oldDMwLT.root'),
+            formulaName = cms.string("mvaOutput_normalization_isoPWR03oldDMwLT"),
+            outputRecord = cms.string("RecoTauTag_tauIdMVAIsoPWR03oldDMwLTv1_mvaOutput_normalization")
         )
     )
 )
