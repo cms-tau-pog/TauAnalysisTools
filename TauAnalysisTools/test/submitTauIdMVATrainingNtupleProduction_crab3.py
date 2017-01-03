50a3fd42b5863d1eb1b053fd3760ba3107d0fc35
anehrkor <alexander.nehrkorn@cern.ch>
2017-01-03 14:02:18 +0100

    Update signal samples: snapshot of DAS from 3.1.2017

--- TauAnalysisTools/test/submitTauIdMVATrainingNtupleProduction_crab3.py
+++ TauAnalysisTools/test/submitTauIdMVATrainingNtupleProduction_crab3.py
@@ -288,8 +288,15 @@ samples[ggSampleName] = {
     'total_files'                        : -1,
     'type'                               : 'SignalMC'
 }
-# currently 15 mass points available
-mssmHiggsMassPoints1 = [ 90, 120, 130, 250, 300, 450, 500, 600, 1000, 1500, 2000, 2300, 2600, 2900, 3200 ]
+vbfSampleName = "vbfHiggs125toTauTau"
+samples[vbfSampleName] = {
+    'datasetpath'                        : '/VBFHToTauTau_M125_13TeV_powheg_pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v1/MINIAODSIM',
+    'files_per_job'                      : 1,
+    'total_files'                        : -1,
+    'type'                               : 'SignalMC'
+}
+# currently 20 mass points available
+mssmHiggsMassPoints1 = [ 80, 90, 100, 120, 130, 250, 300, 400, 450, 500, 600, 700, 1000, 1500, 1600, 2000, 2300, 2600, 2900, 3200 ]
 for massPoint in mssmHiggsMassPoints1:
     ggSampleName = "ggA%1.0ftoTauTau" % massPoint
     samples[ggSampleName] = {
@@ -299,8 +306,8 @@ for massPoint in mssmHiggsMassPoints1:
         'type'                               : 'SignalMC'
     }
 
-# currently 23 mass points available
-mssmHiggsMassPoints2 = [ 90, 100, 120, 140, 160, 200, 250, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1500, 2000, 2300, 2600, 2900, 3200 ]
+# currently 26 mass points available
+mssmHiggsMassPoints2 = [ 90, 100, 110, 120, 140, 160, 200, 250, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1500, 1600, 1800, 2000, 2300, 2600, 2900, 3200 ]
 for massPoint in mssmHiggsMassPoints2:
     bbSampleName = "bbA%1.0ftoTauTau" % massPoint
     samples[bbSampleName] = {
@@ -310,8 +317,8 @@ for massPoint in mssmHiggsMassPoints2:
         'type'                               : 'SignalMC'
     }
 
-# currently 6 mass points available
-ZprimeMassPoints = [ 750, 1250, 1500, 2000, 3000, 3500 ]
+# currently 10 mass points available
+ZprimeMassPoints = [ 500, 750, 1000, 1250, 1500, 2000, 2500, 3000, 3500, 4000 ]
 for massPoint in ZprimeMassPoints:
     sampleName = "Zprime%1.0ftoTauTau" % massPoint
     samples[sampleName] = {
@@ -321,8 +328,8 @@ for massPoint in ZprimeMassPoints:
         'type'                               : 'SignalMC'
     }
 
-# currently 24 mass points available
-WprimeMassPoints = [ 400, 600, 1000, 1200, 1400, 1600, 1800, 2000, 2400, 2600, 2800, 3000, 3400, 3600, 3800, 4000, 4200, 4400, 4600, 4800, 5000, 5200, 5600, 5800 ]
+# currently 25 mass points available
+WprimeMassPoints = [ 400, 600, 1000, 1200, 1400, 1600, 1800, 2000, 2400, 2600, 2800, 3000, 3400, 3600, 3800, 4000, 4200, 4400, 4600, 4800, 5000, 5200, 5400, 5600, 5800 ]
 for massPoint in WprimeMassPoints:
     sampleName = "Wprime%1.0ftoTauNu" % massPoint
     samples[sampleName] = {
