f7efabd45b69f2891da59e1088efcea04681d133
anehrkor <alexander.nehrkorn@cern.ch>
2017-01-12 16:07:56 +0100

    SUSYGluGluToBBToHToTauTau mass point 300 v1 has been invalidated on DAS -> switch to v2 for that sample

--- TauAnalysisTools/test/submitTauIdMVATrainingNtupleProduction_crab3.py
+++ TauAnalysisTools/test/submitTauIdMVATrainingNtupleProduction_crab3.py
@@ -307,7 +307,7 @@ for massPoint in mssmHiggsMassPoints1:
     }
 
 # currently 31 mass points available
-mssmHiggsMassPoints2 = [ 80, 90, 100, 110, 120, 130, 140, 160, 180, 200, 250, 300, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1500, 1600, 1800, 2000, 2300, 2600, 2900, 3200 ]
+mssmHiggsMassPoints2 = [ 80, 90, 100, 110, 120, 130, 140, 160, 180, 200, 250, 350, 400, 450, 500, 600, 700, 800, 900, 1000, 1200, 1400, 1500, 1600, 1800, 2000, 2300, 2600, 2900, 3200 ]
 for massPoint in mssmHiggsMassPoints2:
     bbSampleName = "bbA%1.0ftoTauTau" % massPoint
     samples[bbSampleName] = {
@@ -316,6 +316,14 @@ for massPoint in mssmHiggsMassPoints2:
         'total_files'                        : -1, 
         'type'                               : 'SignalMC'
     }
+# v1 of the mass point 300 is invalid
+bbSampleName = "bbA300toTauTau"
+samples[bbSampleName] = {
+    'datasetpath'                        : '/SUSYGluGluToBBHToTauTau_M-300_TuneCUETP8M1_13TeV-pythia8/RunIISummer16MiniAODv2-PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6-v2/MINIAODSIM',
+    'files_per_job'                      : 1,
+    'total_files'                        : -1, 
+    'type'                               : 'SignalMC'
+}
 
 # currently 11 mass points available
 ZprimeMassPoints = [ 500, 750, 1000, 1250, 1500, 1750, 2000, 2500, 3000, 3500, 4000 ]
