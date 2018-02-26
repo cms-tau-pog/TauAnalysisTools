0b922437a4e20c25555c346f99e8ad0fc4372d9b
Olena Hlushchenko <greyxray@gmail.com>
2018-02-26 11:01:23 +0100

    First skim for the partial MCv2 files

--- TauAnalysisTools/test/submitTauIdMVATrainingNtupleProduction_crab3.py
+++ TauAnalysisTools/test/submitTauIdMVATrainingNtupleProduction_crab3.py
@@ -23,7 +23,7 @@ config = Configuration()
 
 config.section_("General")
 config.General.requestName = '$ui_working_dir'
-config.General.workArea = 'TauIDMVATraining2017_2017MCv2RelVal'
+config.General.workArea = 'Summer17_25ns_2017MCv2_partial'
 
 config.section_("User")
 config.User.voGroup = 'dcms'
@@ -38,7 +38,7 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = $files_per_job
 config.Data.totalUnits = $total_files
-config.Data.outLFNDirBase = '/store/user/ohlushch/TauIDMVATraining2017/Summer17_25ns_2017MCv2RelVal/'
+config.Data.outLFNDirBase = '/store/user/ohlushch/TauIDMVATraining2017/Summer17_25ns_2017MCv2_partial/'
 config.Data.publication = False
 config.Data.allowNonValidInputDataset = True
 
@@ -66,7 +66,7 @@ config.Data.unitsPerJob = $lumis_per_job
 config.Data.totalUnits = $total_lumis
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions12/8TeV/Prompt/Cert_190456-208686_8TeV_PromptReco_Collisions12_JSON.txt'
 #config.Data.runRange = '193093-193999' # '193093-194075'
-config.Data.outLFNDirBase = '/store/user/ohlushch/TauIDMVATraining2017/Summer17_25ns_withCutbased/'
+config.Data.outLFNDirBase = '/store/user/ohlushch/TauIDMVATraining2017/Summer17_25ns_2017MCv2_partial/'
 config.Data.publication = False
 
 config.section_("Site")
