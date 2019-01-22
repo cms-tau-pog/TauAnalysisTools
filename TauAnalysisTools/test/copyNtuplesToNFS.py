748777f3ff02fce5c2c71a4f9831f45d8fc90d67
Olena Hlushchenko <greyxray@gmail.com>
2019-01-22 12:07:36 +0100

    use configs with copy script

--- TauAnalysisTools/test/copyNtuplesToNFS.py
+++ TauAnalysisTools/test/copyNtuplesToNFS.py
@@ -9,18 +9,21 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 # from python_wrappers.subprocessHandler import SubprocessHandler
 
+import yaml
+config = yaml.load(open("config.yaml", 'r'))
+
 # renice -n 19 -u `whoami`
 
 # generic path to dCache where ntuples are stored
-inputPath = '/pnfs/desy.de/cms/tier2/store/user/ohlushch/TauIDMVATraining2018/Autum2018tauId_v1'  # "/pnfs/desy.de/cms/tier2/store/user/ohlushch/TauIDMVATraining2017/Summer17_25ns_2017MCv2_partial_withraw15"
-version = "tauId_v1"
+inputPath = os.path.join(config['pnfs_base'], config['store_base'], config['workarea_base'] + config['version'])  # '/pnfs/desy.de/cms/tier2/store/user/ohlushch/TauIDMVATraining2018/Autum2018tauId_v1'  # "/pnfs/desy.de/cms/tier2/store/user/ohlushch/TauIDMVATraining2017/Summer17_25ns_2017MCv2_partial_withraw15"
+version = config['version']  # "tauId_v1"
 subfolder = ""  # for regular runs use empty string
 sh = SamplesHandles("2018")
 samples = sh.samples
 # samples = sh.getSamplesPU17(subfolder)
 
 # generic path to NFS where we want to copy ntuples to
-outputPath = os.path.join("/nfs/dust/cms/user/glusheno/TauIDMVATraining2018/Autum2018tauId_v1/ntuples/", subfolder)
+outputPath = os.path.join(config['nfs_base'], config['workarea_base'] + config['version'], 'ntuples', subfolder)  # "/nfs/dust/cms/user/glusheno/TauIDMVATraining2018/Autum2018tauId_v1/ntuples/", subfolder)
 
 # loop through dictionary, create directories according to sampleName
 # and copy corresponding ntuples to the directory
@@ -35,16 +38,17 @@ for sampleName, sampleOption in samples.items():
     folderToBeCreated = outputPath + sampleName
     print "folderToBeCreated:", folderToBeCreated
 
-    if os.path.isdir(folderToBeCreated):
-        if rewriteall != True:
+    if os.path.isdir(folderToBeCreated) and len(os.listdir(folderToBeCreated)) != 0:
+        if rewriteall is not True:
 
             tocontinue = False
 
-            if rewriteall == None:
+            if rewriteall is None:
                 reply = None
                 while not any(x == reply for x in ["y", "Y", "n", "N"]):
                     reply = raw_input('rewrite the ' + folderToBeCreated + " folder? [y/n]. Type [Y/N] to apply this to all files.")
-                    if reply == "Y": rewriteall = True
+                    if reply == "Y":
+                        rewriteall = True
                     elif reply == "n":
                         print "Ignorig rewriting", folderToBeCreated
                         tocontinue = True
@@ -56,14 +60,16 @@ for sampleName, sampleOption in samples.items():
                     elif reply != "y":
                         print ("Sorry, invalid input, try again.")
 
-            if rewriteall == False or tocontinue:
+            if rewriteall is False or tocontinue:
                 print "Ignoring", folderToBeCreated
                 continue
 
         print "Overwriting ", folderToBeCreated
         shutil.rmtree(folderToBeCreated)
 
-    os.makedirs(folderToBeCreated)
+    if not os.path.isdir(folderToBeCreated):
+        os.makedirs(folderToBeCreated)
+
     # subfolder = sampleName  #+ "_" + version
     filesToCopy = os.path.join(inputPath, sampleOption['datasetpath'].split('/')[1], '*' + sampleName + '*', "*/*/*.root")
 
